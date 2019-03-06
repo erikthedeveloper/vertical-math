45ab3d843843a716f1f9dd41e53051906ce1b6ab
Erik Aybar <erikthedeveloper@gmail.com>
2019-03-06 06:40:20 -0700

    The first TS conversion! App.js -> App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Platform, StatusBar, StyleSheet, View} from 'react-native';
-import {AppLoading, Asset, Font, Icon} from 'expo';
+// @ts-ignore
+import {AppLoading, Font, Icon} from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
-export default class App extends React.Component {
+export default class App extends React.Component<{skipLoadingScreen: boolean}> {
   state = {
     isLoadingComplete: false,
   };
@@ -28,22 +29,15 @@ export default class App extends React.Component {
   }
 
   _loadResourcesAsync = async () => {
-    return Promise.all([
-      Asset.loadAsync([
-        require('./assets/images/robot-dev.png'),
-        require('./assets/images/robot-prod.png'),
-      ]),
+    await Promise.all([
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
-        // We include SpaceMono because we use it in HomeScreen.js. Feel free
-        // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
       }),
     ]);
   };
 
-  _handleLoadingError = error => {
+  _handleLoadingError = (error: string | Error) => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
     console.warn(error);
