6fcdba04c891ed011f97ce3d801bbe435fd2c9c0
Erik Aybar <erikthedeveloper@gmail.com>
2019-03-07 06:46:12 -0700

    Add RootNavigator persistenceKey

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default class App extends React.Component {
       return (
         <View style={styles.container}>
           {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
+          <AppNavigator persistenceKey="DEV" />
         </View>
       );
     }
