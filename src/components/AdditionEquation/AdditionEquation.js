32781312df8a91599f8b850f8c98a2cb0a4535dc
Erik Aybar <erikthedeveloper@gmail.com>
2019-03-10 10:43:10 -0600

    Extract AdditionEquation utils to math-utils.js

--- src/components/AdditionEquation/AdditionEquation.js
+++ src/components/AdditionEquation/AdditionEquation.js
@@ -2,7 +2,12 @@ import React, {Component} from 'react';
 import {View} from 'react-native';
 import {VerticalNumbers} from '../VerticalNumbers';
 import {NumberRow} from '../NumberRow';
-import {numberToPlaceValue} from '../../utils/math-utils';
+import {
+  numberToPlaceValue,
+  placeValueToIndex,
+  sumArray,
+  sumArrayAtPlaceValue,
+} from '../../utils/math-utils';
 
 export class AdditionEquation extends Component {
   render() {
@@ -41,40 +46,4 @@ export class AdditionEquation extends Component {
       </View>
     );
   }
-}
-
-function sumArray(nums) {
-  return nums.reduce((sum, num) => sum + num);
-}
-
-/**
- * Get the string index for place value. Examples:
- *  1, 0 -> 0
- *  10, 0 -> 1
- *  100, 1 -> 1
- *  1000, 1 -> 2
- *  1000, 5 -> null
- */
-function placeValueToIndex(num, placeValue) {
-  const numPlaceValue = numberToPlaceValue(num);
-  return !Number.isInteger(placeValue) || placeValue > numPlaceValue
-    ? null
-    : numPlaceValue - placeValue;
-}
-
-function sumArrayAtPlaceValue(addends, placeValue) {
-  return sumArray(
-    addends.map(number => numberAtPlaceValue(number, placeValue))
-  );
-}
-
-/**
- * Get the single digit number at place value. Examples:
- *   159, 0 -> 9
- *   159, 1 -> 5
- *   159, 2 -> 0
- */
-function numberAtPlaceValue(number, placeValue) {
-  const result = Number(String(number)[placeValueToIndex(number, placeValue)]);
-  return Number.isInteger(result) ? result : 0;
 }
--- src/utils/math-utils.js
+++ src/utils/math-utils.js
@@ -13,3 +13,39 @@ function valueAtIndex(number, i) {
   const aString = String(number);
   return Number(aString[i]) * Math.pow(10, aString.length - 1 - i);
 }
+
+export function sumArray(nums) {
+  return nums.reduce((sum, num) => sum + num);
+}
+
+/**
+ * Get the string index for place value. Examples:
+ *  1, 0 -> 0
+ *  10, 0 -> 1
+ *  100, 1 -> 1
+ *  1000, 1 -> 2
+ *  1000, 5 -> null
+ */
+export function placeValueToIndex(num, placeValue) {
+  const numPlaceValue = numberToPlaceValue(num);
+  return !Number.isInteger(placeValue) || placeValue > numPlaceValue
+    ? null
+    : numPlaceValue - placeValue;
+}
+
+export function sumArrayAtPlaceValue(addends, placeValue) {
+  return sumArray(
+    addends.map(number => numberAtPlaceValue(number, placeValue))
+  );
+}
+
+/**
+ * Get the single digit number at place value. Examples:
+ *   159, 0 -> 9
+ *   159, 1 -> 5
+ *   159, 2 -> 0
+ */
+function numberAtPlaceValue(number, placeValue) {
+  const result = Number(String(number)[placeValueToIndex(number, placeValue)]);
+  return Number.isInteger(result) ? result : 0;
+}
